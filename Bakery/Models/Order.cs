ff1fa733a3c93cf0f40b52d6bea57c42d8398fe9
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-05 09:46:42 -0800

    create an Order class

---
+++ Bakery/Models/Order.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Order
+  {
+    public string Description { get; set; }
+    public string Title { get; set; }
+    public int Id { get; }
+    public decimal Price { get; set; }
+
+    public string Date { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order(string title, string description, string date)
+    {
+      Title = title;
+      Description = description;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+  }
+}
