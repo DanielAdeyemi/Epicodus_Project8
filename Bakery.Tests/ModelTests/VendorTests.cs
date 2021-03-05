73eee34ac4d12aad7e92d0217e891b747cbe6049
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-05 10:25:38 -0800

    create Vendor object and fail test #1 for it

---
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test", "Vendor");
+      Assert.AreEqual("AMC", newVendor.Name);
+    }
+  }
+}
---
+++ Bakery/Models/Vendor.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> {};
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
